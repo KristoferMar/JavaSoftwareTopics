84801a95d1acf69a67e0eb20b9c35828db41d90d
KristoferMar <kristofer@brostu.dk>
2019-01-29 12:49:36 +0100

    Inline if statements in java.

---
+++ SoftwareTopics/src/smallTopics/InlineIfStatement.java
@@ -0,0 +1,17 @@
+package smallTopics;
+
+public class InlineIfStatement {
+	
+	/*
+	 * This is an inline if statement in JAVA.
+	 */
+	
+	public static void main(String[] args) {
+	
+		// "your condition"? "step if true":"step if condition fails"
+		
+
+
+	}
+
+}
